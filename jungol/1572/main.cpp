a8c83359effdb617f8e8a51c73ea08ef6deefdfc
asdfjk123 <minjae20000514@gmail.com>
2026-05-07 19:18:34 +0900

    구현 방법 기록 및 베이스 코드 작성

--- jungol/1572/main.cpp
+++ jungol/1572/main.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
+
+typedef struct Space {
+    bool up_lined = false, down_lined = false, left_lined = false, right_lined = false; // 정사각형 공간의 4개의 면의 선이 그려졌는지 여부
+    bool is_checked; // 해당 면은 이미 조사가 완료되었는지의 여부
+};
+
+int N, x_input, y_input, result;
+int dots[10000][10000];
+Space spaces[9999][9999];
+
+void input() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> x_input >> y_input;
+        // TODO: dots 에 반영
+        // TODO: Spaces 에 선이 그어진 여부 반영
+    }
+}
+
+void calculate() {
+
+}
+
+void output() {
+    cout << result << endl;
+}
 
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
+    input();
+    calculate();
+    output();
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
