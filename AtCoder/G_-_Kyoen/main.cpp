d602ae49b8d2a209e6a6075be7e305bdb24a4b4e
asdfjk123 <minjae20000514@gmail.com>
2026-06-24 16:21:46 +0900

    [FEATURE] 문제 정의

--- AtCoder/G_-_Kyoen/main.cpp
+++ AtCoder/G_-_Kyoen/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+/*
+ * 원의 둘레 중 정수의 좌표를 가진 점을 998244353 이라는 값으로 나눈 나머지를 구하라.
+ * 중심점은 (A/C, B/C) 이며 반지름은 root(N)/C 이다.
+ * N = P^E 의 모든 곱
+ */
+
 using namespace std;
 
 void input() {
