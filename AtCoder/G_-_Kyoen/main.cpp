6d1b31309af19f664a0b0981fe82aaf158109ccd
asdfjk123 <minjae20000514@gmail.com>
2026-06-24 18:22:45 +0900

    [FEATURE] 아이디어 메모

--- AtCoder/G_-_Kyoen/main.cpp
+++ AtCoder/G_-_Kyoen/main.cpp
@@ -6,9 +6,28 @@
  * N = P^E 의 모든 곱
  */
 
+/*
+ * 문제 풀이 원리
+ * 원을 둘러싸는 정사각형을 그린 다음, 정사각형과 인접한 원 둘레의 점만 조사해서 찾아내면 되지 않을까?
+ */
+
 using namespace std;
 
+int A, B, C, M;
+vector<pair<int, int>> PE;
+int result;
+
+
 void input() {
+    cin >> A >> B >> C >> M;
+
+    int P, E;
+
+    for (int i = 0; i < M; i++) {
+        cin >> P >> E;
+        PE.emplace_back(P, E);
+    }
+
 
 }
 
@@ -17,13 +36,17 @@ void process() {
 }
 
 void output() {
-
+    cout << result << endl;
 }
 
 int main() {
 
     iostream::sync_with_stdio(false);
     cin.tie(NULL);
+
+    input();
+    process();
+    output();
 
     return 0;
 }
