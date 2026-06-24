085006d6e14b8c4c072524a287f4b4b8e9da9189
asdfjk123 <minjae20000514@gmail.com>
2026-06-24 14:51:32 +0900

    [FEATURE] 베이스 코드 작성

---
+++ AtCoder/G_-_Kyoen/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void input() {
+
+}
+
+void process() {
+
+}
+
+void output() {
+
+}
+
+int main() {
+
+    iostream::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
